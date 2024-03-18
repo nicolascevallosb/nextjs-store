777d26408ba7ce7d4b31384ddc90588e90a95dc1
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-17 21:51:24 -0500

    Adding static files

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./Description.module.sass"
 
 export const Description = () => {
     return(
-        <section>
-            <h1>Description</h1>
+        <section className={styles.description}>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <div className={styles.description__text}>
+                <h2>Description</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor optio debitis quod perspiciatis possimus facere modi, praesentium enim autem velit! Incidunt deserunt explicabo accusamus soluta placeat sequi a quisquam provident.</p>
+            </div>
         </section>
     );
 }
