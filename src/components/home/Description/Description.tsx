d4ca6775967952c124dac2926a85445df56ec07c
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-17 22:08:15 -0500

    Managing and optimizing img with Next Image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./Description.module.sass"
+import Image from "next/image";
 
 export const Description = () => {
     return(
         <section className={styles.description}>
-            <img src="/images/description.jpeg" alt="products marketplace" />
+            <Image
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300}/>
             <div className={styles.description__text}>
                 <h2>Description</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor optio debitis quod perspiciatis possimus facere modi, praesentium enim autem velit! Incidunt deserunt explicabo accusamus soluta placeat sequi a quisquam provident.</p>
