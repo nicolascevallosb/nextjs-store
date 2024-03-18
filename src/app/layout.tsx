a4af35211cb4c8294b3bbddd3fe68281e87c023d
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-18 16:50:08 -0500

    Updating font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import "app/sass/globals.sass";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ 
+  weight: ["100", "300", "500", "700"],
+  subsets: ["latin"],
+ });
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CategoryProps {
+    params: {
+        category: string,
+    }
+}
+
+export default function Category(props: CategoryProps) {
+    const {
+        category,
+    } = props.params
+
+    return(
+        <h1>{category}</h1>
+    );
+}
