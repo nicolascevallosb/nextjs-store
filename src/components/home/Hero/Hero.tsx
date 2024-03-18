e0773523034d49972d2ff0e9debb75cc0656f902
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-17 19:44:08 -0500

    Trying CSS modules

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./Hero.module.css"
 
 export const Hero = () => {
     return(
-        <section>
-            <h1>Hero</h1>
+        <section className={styles.hero}>
+            <h1>Maxi Emporium</h1>
+            <h2>Empowering Your Tomorrow, Today!</h2>
         </section>
     );
 }
