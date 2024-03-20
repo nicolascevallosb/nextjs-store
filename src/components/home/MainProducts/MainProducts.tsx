8537193c9083ae32f386f80329e94eee46573405
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-20 14:34:06 -0500

    Getting products with Shopify API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`, {
+        headers: new Headers ({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+    });
+    const data = await response.json();
+    return(data);
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts();
+    console.log(products);
+    
     return(
         <section>
             <h1>Main Products</h1>
