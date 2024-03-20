ac5c4fb8d817b80de8818a584cd50e9f6728a729
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-20 15:48:23 -0500

    Fixing categories

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,14 +1,12 @@
 interface CategoryProps {
     params: {
         categories: string[],
-        searchParams?: string,
-    }
+    },
+    searchParams?: string,
 }
 
 export default function Category(props: CategoryProps) {
-    const {
-        categories,
-    } = props.params
+    const { categories } = props.params
 
     return(
         <h1>Store: {categories}</h1>
