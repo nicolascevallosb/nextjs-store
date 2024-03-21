334ea7689bd653f22cd5fe67dc6871ee74a32e91
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-21 17:06:51 -0500

    Improving SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Maxi Emporium ⚔",
+  description: "Welcome to Maxi Emporium, an ecommerce from another century",
+  keywords: ["ecommerce", "maxi", "emporium", "technology", "store"]
+}
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,14 +8,28 @@ interface ProductPageProps {
     }
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+    const id = searchParams.id;
+    const products = await getProducts(id);
+    const product = products[0];
+
+    return({
+        title: product.title,
+        description: product.description,
+        keywords: product.tags,
+        openGraph: {
+            images: [product.image]
+        }
+    });
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
     const id = searchParams.id;
     const products = await getProducts(id);
     const product = products[0];
 
-    if(!id) {
+    if(!id)
         redirect('/store');
-    }
 
     return (
         <ProductView product={product} />
