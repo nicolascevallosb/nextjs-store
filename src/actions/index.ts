97f7a3337f11e9e762d6f0654781ae99ff195feb
nicolascevallosb <nicolascevallosb@gmail.com>
2024-03-21 22:38:01 -0500

    Adding signup action/Creating clients

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,21 @@
 "use server";
 
-export const handleCreateUser = (formData) => {
-    console.log(formData)
+import { GraphQLClientSingleton } from "app/graphql";
+import { createUserMutation } from "app/graphql/mutations/createUserMutation";
 
+export const handleCreateUser = async (formData: FormData) => {
+    const formDataObject = Object.fromEntries(formData);
+    delete formDataObject["password_confirmation"];
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+    const variables = {
+        input: {
+            ...formDataObject,
+            phone: '+593' + formDataObject.phone
+        }
+    };
+
+    const { customerCreate } = await graphqlClient.request(createUserMutation, variables);
+    const { customerUserErrors, customer } = customerCreate;
+    console.log(customer);
+    console.log(customerUserErrors);
 }
--- src/components/signup/NewAccountForm/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm/NewAccountForm.tsx
@@ -8,18 +8,18 @@ export const NewAccountForm = () => {
     const [errors, setErrors] = useState<string[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        handleCreateUser(formData);
+        await handleCreateUser(formData);
     }
 
     return (
         <div className={styles.new_account_form}  >
             <h1 className={styles.new_account_form_title}>New Account</h1>
             <form className={styles.new_account_form_form} onSubmit={handleSubmit}>
-                <input type="text" name="first_name" placeholder="Name" disabled={loading} />
-                <input type="text" name="last_name" placeholder="Lastname" disabled={loading} />
+                <input type="text" name="firstName" placeholder="Name" disabled={loading} />
+                <input type="text" name="lastName" placeholder="Lastname" disabled={loading} />
                 <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
                 <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
                 <input type="password" name="password" placeholder="password" disabled={loading} />
